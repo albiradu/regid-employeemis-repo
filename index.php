ef6f2f4edd37caf2205fd297316b083dbda26add
albertine <albiradu@gmail.com>
2025-01-23 11:15:34 +0200

    employed, the form is added successfully

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Staff MIS</title>
 </head>
 <body>
-    
+<h1>Welcome to new staff MIS</h1>
+    <a href="registration.php">Go to Registration Page</a>
+
 </body>
 </html>
